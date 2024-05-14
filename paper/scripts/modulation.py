Fix sign in 3rd order grav. accel.

paper/scripts/modulation.py
@@ -8,7 +8,7 @@ def gravity(
     phi = phase(x, t, k, omega)
     term1 = -a * k * np.cos(phi)
     term2 = -a * k * np.cos(phi) * np.exp(a * k * np.cos(phi))
-    term3 = -((a * k * np.sin(phi)) ** 2) * np.exp(a * k * np.cos(phi))
+    term3 = ((a * k * np.sin(phi)) ** 2) * np.exp(a * k * np.cos(phi))
     if order == 0:
         return g0 * np.ones_like(x)
     elif order == 1:

paper/scripts/plot_gravity_modulation.py
@@ -14,7 +14,7 @@ def g_eulerian(g, psi, a, k, order=3):
         res = g * (1 - a * k * np.cos(psi) * np.exp(k * eta))
     else:
         res = g * (
-            1 - a * k * np.exp(k * eta) * (np.cos(psi) + a * k * np.sin(psi) ** 2)
+            1 - a * k * np.exp(k * eta) * (np.cos(psi) - a * k * np.sin(psi) ** 2)
         )
     return res
 
@@ -46,11 +46,8 @@ def plot_fig_apparent_gravity():
 
     g = 9.8
 
-    g_eulerian_1 = g_eulerian(g, phase, a_L, k_L, order=1)
-    g_eulerian_2 = g_eulerian(g, phase, a_L, k_L, order=2)
-    g_eulerian_3 = g_eulerian(g, phase, a_L, k_L, order=3)
-    g_lagrangian_3 = g_lagrangian(g, phase, a_L, k_L, order=3)
-    # g_lagrangian_4 = g_lagrangian(g, phase, a_L, k_L, order=4) # Essentially the same as 3rd order; omit
+    g_E1 = g_eulerian(g, phase, a_L, k_L, order=1)
+    g_E3 = g_eulerian(g, phase, a_L, k_L, order=3)
 
     fig = plt.figure(figsize=(14, 6))
     ax1 = fig.add_subplot(121)
@@ -63,10 +60,8 @@ def plot_fig_apparent_gravity():
         0, 1.02, "(b)", ha="left", va="bottom", transform=ax2.transAxes, fontsize=20
     )
 
-    ax1.plot(phase, g_eulerian_1 / g, label=r"Eulerian, 1$^{st}$ order", lw=2)
-    ax1.plot(phase, g_eulerian_2 / g, label=r"Eulerian, 2$^{nd}$ order", lw=2)
-    ax1.plot(phase, g_eulerian_3 / g, label=r"Eulerian, 3$^{rd}$ order", lw=2)
-    ax1.plot(phase, g_lagrangian_3 / g, label=r"Lagrangian, 3$^{rd}$ order", lw=2)
+    ax1.plot(phase, g_E1 / g, "k-", label=r"1$^{st}$ order", lw=2)
+    ax1.plot(phase, g_E3 / g, "r-", label=r"3$^{rd}$ order", lw=2)
     ax1.legend()
     ax1.set_xlabel("Phase (rad.)")
     ax1.set_ylabel(r"$\widetilde g/g$")
@@ -77,33 +72,32 @@ def plot_fig_apparent_gravity():
     ax1.set_xticklabels(["0", r"$\pi/2$", r"$\pi$", r"$3\pi/2$", r"$2\pi$"])
 
     # Evaluate Eulerian and Lagrangian gravity vs. ak
-    g_E1 = []
-    g_E2 = []
-    g_E3 = []
-    g_L3 = []
+    g_E1_min, g_E1_max = [], []
+    g_E3_min, g_E3_max = [], []
     ak_range = np.arange(0, 0.41, 0.01)
     for ak in ak_range:
         a_L = ak / k_L
-        g_E1.append(np.mean(g_eulerian(g, phase, a_L, k_L, order=1)))
-        g_E2.append(np.mean(g_eulerian(g, phase, a_L, k_L, order=2)))
-        g_E3.append(np.mean(g_eulerian(g, phase, a_L, k_L, order=3)))
-        g_L3.append(np.mean(g_lagrangian(g, phase, a_L, k_L, order=3)))
-    g_E1 = np.array(g_E1) / g
-    g_E2 = np.array(g_E2) / g
-    g_E3 = np.array(g_E3) / g
-    g_L3 = np.array(g_L3) / g
+        g_E1_min.append(np.min(g_eulerian(g, phase, a_L, k_L, order=1)))
+        g_E1_max.append(np.max(g_eulerian(g, phase, a_L, k_L, order=1)))
+        g_E3_min.append(np.min(g_eulerian(g, phase, a_L, k_L, order=3)))
+        g_E3_max.append(np.max(g_eulerian(g, phase, a_L, k_L, order=3)))
+    g_E1_min = np.array(g_E1_min) / g
+    g_E3_min = np.array(g_E3_min) / g
+    g_E1_max = np.array(g_E1_max) / g
+    g_E3_max = np.array(g_E3_max) / g
 
-    ax2.plot(ak_range, g_E1, label=r"Eulerian, 1$^{st}$ order", lw=2)
-    ax2.plot(ak_range, g_E2, label=r"Eulerian, 2$^{nd}$ order", lw=2)
-    ax2.plot(ak_range, g_E3, label=r"Eulerian, 3$^{rd}$ order", lw=2)
-    ax2.plot(ak_range, g_L3, label=r"Lagrangian, 3$^{rd}$ order", lw=2)
+    ax2.plot(ak_range, g_E1_max, "k-", label=r"1$^{st}$ order, max.", lw=2)
+    ax2.plot(ak_range, g_E1_min, "k:", label=r"1$^{st}$ order, min.", lw=2)
+    ax2.plot(ak_range, g_E3_max, "r-", label=r"3$^{rd}$ order, max.", lw=2)
+    ax2.plot(ak_range, g_E3_min, "r:", label=r"3$^{rd}$ order, min.", lw=2)
+    ax2.legend()
     ax2.plot([0, 0.4], [1, 1], "k--")
     ax2.grid()
     ax2.set_xlabel(r"$\varepsilon_L$")
     ax2.set_ylabel(r"$\overline{\widetilde g}/g$")
     ax2.set_xlim(0, 0.4)
     ax2.set_xticks([0, 0.1, 0.2, 0.3, 0.4])
-    ax2.set_ylim(0.8, 1.05)
+    # ax2.set_ylim(0.8, 1.05)
 
     fig.subplots_adjust(left=0.06, right=0.96)
     plt.savefig("../figures/fig_gravity_modulation_2panel.png", dpi=200)

paper/scripts/plot_wavenumber_modulation.py
@@ -40,20 +40,24 @@ def plot_fig_wavenumber_modulation():
 
     # Evaluate wa enumber modulation as function of ak
     ak_range = np.arange(0, 0.41, 0.01)
-    k1_mean, k1_max = [], []
-    k2_mean, k2_max = [], []
+    k1_mean, k1_max, k1_min = [], [], []
+    k2_mean, k2_max, k2_min = [], [], []
     for ak in ak_range:
         k1 = 1 + ak * np.cos(phase)
         k2 = np.exp(ak * np.cos(phase) * np.exp(ak * np.cos(phase)))
         k1_mean.append(np.mean(k1))
         k1_max.append(np.max(k1))
+        k1_min.append(np.min(k1))
         k2_mean.append(np.mean(k2))
         k2_max.append(np.max(k2))
+        k2_min.append(np.min(k2))
 
-    ax2.plot(ak_range, k1_max, "k-", label="L-H&S, max", lw=2)
+    ax2.plot(ak_range, k1_max, "k-", label="L-H&S, max.", lw=2)
     ax2.plot(ak_range, k1_mean, "k--", label="L-H&S, mean", lw=2)
-    ax2.plot(ak_range, k2_max, "r-", label="This paper, max", lw=2)
+    ax2.plot(ak_range, k1_min, "k:", label="L-H&S, min.", lw=2)
+    ax2.plot(ak_range, k2_max, "r-", label="This paper, max.", lw=2)
     ax2.plot(ak_range, k2_mean, "r--", label="This paper, mean", lw=2)
+    ax2.plot(ak_range, k2_min, "r:", label="This paper, min.", lw=2)
     ax2.legend(loc="upper left")
     ax2.grid()
     ax2.set_xlabel(r"$\varepsilon_L$")